e0aa32e596c008cffabf2ad69badd225fc8f0524
elchanan cohen <0548500232e@gmail.com>
2023-11-20 14:24:19 +0200

    conection to postgres

---
+++ src/PostgreSQL/PostgreSQL.ts
@@ -0,0 +1,46 @@
+
+// import { Client } from 'pg';
+// export default async function connectToDatabase() {
+//   const connectionString = `postgres://${process.env.DATABASE_USERNAME}:${process.env.PASSWORD_TO_POSTGRES}@dpg-cldimnmg1b2c73f7ul1g-a.oregon-postgres.render.com/users_cwmw`;
+
+//   const client = new Client({
+//     connectionString: connectionString,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   });
+
+//   try {
+//     await client.connect();
+//     console.log('Connected to PostgreSQL database');
+//   } catch (error) {
+//     console.error('Error connecting to PostgreSQL:', (error as Error).message);
+//   } finally {
+//     await client.end();
+//   }
+// }
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: `postgres://${process.env.DATABASE_USERNAME}:${process.env.PASSWORD_TO_POSTGRES}@dpg-cldimnmg1b2c73f7ul1g-a.oregon-postgres.render.com/users_cwmw`,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+async function checkConnection() {
+  let client;
+
+  try {
+    client = await pool.connect();
+    console.log('Connected to PostgreSQL database');
+  } catch (error) {
+    console.error('Error connecting to PostgreSQL:', (error as Error).message);
+  } finally {
+    if (client) {
+      client.release();
+    }
+  }
+}
+checkConnection();
+export default pool
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ const app = express();
 import chalk from "chalk";
 import cors from "cors";
 require('dotenv').config();
-
+import pool from "./PostgreSQL/PostgreSQL";
 
 app.use(cors());
 app.use(express.json());
@@ -13,4 +13,5 @@ const PORT = 8181;
 
 app.listen(PORT, async () => {
   console.log(chalk.blueBright(`Server listening on port: ${PORT}`));
+   pool
 });
