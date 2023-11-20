fcc51e69497d12ece6febbc68acf9723ad0d88a1
elchanan cohen <0548500232e@gmail.com>
2023-11-20 15:35:36 +0200

    register_user

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import Jwt from "jsonwebtoken"
-import { UserInterface } from "./userInterface";
+import { UserInterface } from "../interfaces/userInterface";
 import { registerService } from "./userService";
 
 
 export const registerController = async (req: Request, res: Response) => {
     try {
       const user :UserInterface = req.body;
+      console.log(user)
       const users = await registerService(user);
       
       if (users) return res.status(200).json({users : users});
