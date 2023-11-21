f0055bccf7509f8bf73373f85b6d4e6f83454f79
רפאל אדרי <you@example.com>
2023-11-21 10:05:32 +0200

    add jwt

--- src/users/usersRoutes.ts
+++ src/users/usersRoutes.ts
@@ -1,9 +1,9 @@
 import  express  from "express";
 import { createUsersTable } from "./userDal";
-import { registerController } from "./userController";
+import { registerController,loginController } from "./userController";
 const userRouter = express.Router();
 createUsersTable()
 userRouter.post("/register",registerController);
-userRouter.post('/login');
+userRouter.post('/login',loginController);
 
 export default userRouter
