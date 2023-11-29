f8a4f18aa2a7d778e8795aa09ea741a05f1771aa
רפאל אדרי <you@example.com>
2023-11-29 15:46:01 +0200

    cors the end meby

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -2,10 +2,12 @@ import * as dotenv from 'dotenv';
 
 dotenv.config 
 const FRONT:string = process.env.BASE_URL_FRONT as string;
+const STORE:string = process.env.BASE_URL_STORE as string;
 
 export const allowedOrigins:string[] = [
     'http://localhost:8080',
     'http://127.0.0.1:8080',
-    'https://final-project-front.onrender.com',
+    FRONT,
+    STORE
 ]
 
--- src/config/corsConfigs.ts
+++ src/config/corsConfigs.ts
@@ -1,35 +1,17 @@
 import { allowedOrigins } from "./allowedOrigins"
-import { Request,Response } from "express"; 
+import { Request } from "express"; 
 
-// export const corsConfigs = {
-//     origin: (origin:string | undefined, callback:(err: Error | null, allow?: boolean) => void) => {
-//         console.log(`${origin}  cors`);
-//         console.log(allowedOrigins.includes(String(origin)) +' cors');
-//         console.log(allowedOrigins)
-        
-//         if (allowedOrigins.includes('https://final-project-front.onrender.com')   ) {
-//             callback(null, true)
-//         } else {
-//             callback(new Error('origin not allowed by Cors'),false)
-//         }
-//     },
-//     optionsSuccessStatus: 200,
-// }
 
 export const corsConfigs = (req:Request,callback:(err: Error | null, corsOptions: object) => void) => {
     
-        console.log(`${String(req.header('Origin'))}  cors`);
-        console.log(allowedOrigins.includes(String(req.header('Origin'))) +' cors');
-        console.log(allowedOrigins)
     let corsOptions:object ;
 
-    if (allowedOrigins.indexOf('https://final-project-front.onrender.com') !== -1) {
-        corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    if (allowedOrigins.includes(String(req.header('Origin')))) {
+        corsOptions = { origin: true } 
       } else {
-        corsOptions = { origin: false } // disable CORS for this request
+        corsOptions = { origin: false } 
         throw new Error('origin not allowed by Cors')
       }
-      console.log(corsOptions)
       callback(null, corsOptions) 
     }
 
