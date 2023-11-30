4fb97b0dc37822de9f90ee800b3152e7495fab94
unknown <rs315744656@gmail.com>
2023-11-30 13:06:50 +0200

    refael

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ import connectToDatabase from "./mongoDB/mongoConnection";
 dotenv.config();
 
 app.use(cors());
-app.use(morgan("tiny"));
+app.use(morgan('tiny'))
 app.use(express.json());
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
@@ -23,7 +23,7 @@ const PORT = process.env.PORT;
 
 app.listen(PORT, async () => {
   console.log(chalk.blueBright(`Server listening on port: ${PORT}`));
-  pool;
-  connectToDatabase();
+   pool
+   connectToDatabase(); 
 });
 export default app;
