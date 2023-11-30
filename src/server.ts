5ec5faba8f3e17a8ada3fa18a51473b27aa13e91
elchanan cohen <0548500232e@gmail.com>
2023-11-30 09:40:35 +0200

    cors fixed

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import cors from "cors";
 import * as dotenv from 'dotenv';
 import { checkConnection } from "./PostgreSQL/PostgreSQL";
 import connectToDatabase from "./mongoDB/mongoConnection";
-// import  { corsConfigs }   from "./config/corsConfigs";
+
 
 
 dotenv.config();
