8685740ed3eb947654c4c201fc8d5ad94bd47058
ishai-code <141723065+ishai-code@users.noreply.github.com>
2023-12-03 14:38:14 +0200

    Update userController.test.ts

--- __tests__/userController.test.ts
+++ __tests__/userController.test.ts
@@ -1,29 +1,43 @@
 import request from "supertest";
-import { Request } from "express";
 import app from "../src/server";
 
-const server=request('http://localhost:8080')
+const server = request(app);
+
 describe('User Controller', () => {
-  let userId:Number;
+  let userId: Number;
+
   test('register a new user', async () => {
     const existingUser = {
-      "email":"elchide1@gmail.com","password":"1234567e","isadmin":"false"
+      "email": "elchide1@gmail.com",
+      "password": "1234567e",
+      "isadmin": "false"
     };
 
-    const res = await server
-      .post('/api/users/register')
-      .send(existingUser)
-      .timeout(10000)
-      .expect(200);
+    try {
+      const res = await server
+        .post('/api/users/register')
+        .send(existingUser)
+        .timeout(10000)
+        .expect(200);
+
+      console.log('Registration Response:', res.status, res.body);
+
       expect(res.body.users).toBeDefined();
-      const { users, accessToken, refreshToken } = res.body; 
+      const { users, accessToken, refreshToken } = res.body;
       userId = users.id;
-      expect(users).toBeDefined(); 
-      expect(users.id).toBeDefined(); 
+
+      expect(users).toBeDefined();
+      expect(users.id).toBeDefined();
       expect(users.email).toEqual(existingUser.email);
 
       const deleteResponse = await server
-      .delete(`/api/users/${userId}`)
-      .expect(200);
+        .delete(`/api/users/${userId}`)
+        .expect(200);
+
+      console.log('Delete Response:', deleteResponse.status, deleteResponse.body);
+    } catch (error) {
+      console.error('Test Error:', error);
+      throw error;  // Rethrow the error to fail the test
+    }
   });
 });
