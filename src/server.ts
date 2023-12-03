f34d7ed09e9a771744e24e5356292e94c16d5e87
ishai-code <141723065+ishai-code@users.noreply.github.com>
2023-12-03 15:13:27 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -26,4 +26,19 @@ app.listen(PORT, async () => {
   checkConnection()
   connectToDatabase();
 });
+const server = app.listen(PORT, async () => {
+  console.log(chalk.blueBright(`Server listening on port: ${PORT}`));
+  checkConnection();
+  connectToDatabase();
+});
+
+// Gracefully shut down the server
+process.on('SIGINT', () => {
+  console.log('Server is shutting down...');
+  server.close(() => {
+    console.log('Server has shut down.');
+    process.exit(0);
+  });
+});
+
 export default app;
