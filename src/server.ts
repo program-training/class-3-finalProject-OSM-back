3e3467599081044df4848313795b5999c5e13349
ishai-code <141723065+ishai-code@users.noreply.github.com>
2023-12-04 13:35:04 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,34 @@
-import express from "express";
-import  userRouter  from "./users/usersRoutes";
-import orderRouter from "./orders/orderRoutes";
-import chalk from "chalk";
-import morgan from "morgan";
-import cors from "cors";
+// src/server.ts
+import express from 'express';
+import http from 'http'; // Import the HTTP module
+
+import userRouter from './users/usersRoutes';
+import orderRouter from './orders/orderRoutes';
+import chalk from 'chalk';
+import morgan from 'morgan';
+import cors from 'cors';
 import * as dotenv from 'dotenv';
-import { checkConnection } from "./PostgreSQL/PostgreSQL";
-import connectToDatabase from "./mongoDB/mongoConnection";
-
-
+import { checkConnection } from './PostgreSQL/PostgreSQL';
+import connectToDatabase from './mongoDB/mongoConnection';
 
 dotenv.config();
 const app = express();
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/users", userRouter);
-app.use("/api/orders", orderRouter);
+app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
+
+const server = http.createServer(app); // Create an HTTP server
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, async () => {
+server.listen(PORT, async () => {
   console.log(chalk.blueBright(`Server listening on port: ${PORT}`));
-  checkConnection()
+  checkConnection();
   connectToDatabase();
 });
-export default app;
+
+export default server; // Export the server object
+
