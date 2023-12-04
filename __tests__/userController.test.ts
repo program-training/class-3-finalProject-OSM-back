6895484ac6efd6b777fd7821830380627d74f2e7
ishai-code <141723065+ishai-code@users.noreply.github.com>
2023-12-04 14:12:23 +0200

    Update userController.test.ts

--- __tests__/userController.test.ts
+++ __tests__/userController.test.ts
@@ -1,10 +1,17 @@
- import request from "supertest";
+import request from "supertest";
 import app from "../src/server";
 
 const server = request(app);
 
 describe('User Controller', () => {
-  let userId: Number;
+  let userId: number;
+
+  afterEach(async () => {
+    if (userId) {
+      // Delete the user after each test
+      await server.delete(`/api/users/${userId}`).expect(200);
+    }
+  });
 
   test('register a new user', async () => {
     const existingUser = {
@@ -29,16 +36,16 @@ describe('User Controller', () => {
       expect(users).toBeDefined();
       expect(users.id).toBeDefined();
       expect(users.email).toEqual(existingUser.email);
-
-      const deleteResponse = await server
-        .delete(`/api/users/${userId}`)
-        .expect(200);
-
-      console.log('Delete Response:', deleteResponse.status, deleteResponse.body);
     } catch (error) {
       console.error('Test Error:', error);
       throw error;  // Rethrow the error to fail the test
     }
   });
+
+  afterAll(async () => {
+    // Close the app after all tests
+    await app.close();
+  });
 });
+
 
