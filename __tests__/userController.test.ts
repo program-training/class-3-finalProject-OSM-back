7eafafcecf7af86f479214f60232cbfeb920e7f9
ishai-code <141723065+ishai-code@users.noreply.github.com>
2023-12-04 10:12:25 +0200

    Update userController.test.ts

--- __tests__/userController.test.ts
+++ __tests__/userController.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import app from "../src/server";
 
-const server = request(app);
+const server = request('http://localhost:3000');
 
 describe('User Controller', () => {
   let userId: Number;
